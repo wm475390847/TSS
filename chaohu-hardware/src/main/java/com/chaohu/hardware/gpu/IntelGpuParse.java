1b615875a7e6ecd0602dbc0e953f03f9dd142b27
王敏 <wangmin.wm@shuwen.com>
2022-11-07 12:00:53 +0800

    添加代码

---
+++ chaohu-hardware/src/main/java/com/chaohu/hardware/gpu/IntelGpuParse.java
@@ -0,0 +1,20 @@
+package com.chaohu.hardware.gpu;
+
+import com.chaohu.hardware.pojo.GpuInfo;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author wangmin
+ * @date 2022/4/14 09:15
+ */
+@Slf4j
+public class IntelGpuParse extends BaseGpuParse {
+
+    @Override
+    protected Optional<List<GpuInfo>> find(String command) {
+        return Optional.empty();
+    }
+}
